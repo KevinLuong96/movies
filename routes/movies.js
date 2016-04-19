ae905f845586b12b19dc6c60c7ae27f0218acf17
Kevin Luong <k5luong@uwaterloo.ca>
2016-04-19 15:54:56 -0400

    added multiple movie search capability

--- routes/movies.js
+++ routes/movies.js
@@ -7,45 +7,52 @@ var router = express.Router();
 var bing = require('node-bing-api')({ accKey: 'hP4kNCbLCTBmE1FnzDozKj2m9mFgOosKUcXS6lKAznE' });
 //variable declarations
 var app = express();
-var movie, url;
-var movieInfo= {};
+var url, title;
+var movies = [];
+var movieImages = [];
+var movieInfo = [];
+var counter = 0;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-function getMovieImage(title, callback){
+function getMovieImage(titles, index, callback){
   // takes the title of a movie, searches the bing api for related image
   // and adds it to an object holding movie info
-  movieInfo.Image = [];
-  bing.images(title + ' movie', {
+  console.log('started');
+  bing.images(titles[index] + ' movie', {
     top: 4,
     imageFilters: {
       aspect: 'wide'
     },
     face: 'other'
   }, function(err, res, body){
-    if(err){ 
-      throw err;
-    } else{
-      body.d.results.forEach(function(movie){
-        movieInfo.Image.push(movie.MediaUrl);
+    // if(err){ 
+    //   throw err;
+    // } else{
+      console.log(body.d + body['d']);
+      body['d'].results.forEach(function(movie){
+        console.log('image added!' + movie.MediaUrl);
+        movieImages[index].push(movie.MediaUrl);
       }); // foreach movie
-      callback(movieInfo);
-    } 
+      callback();
+    // } 
   }); //callback function
 }
 
-function getMovieInfo(title,callback){
-  // takes the title of a movie and returns an object containing information about it
-  url = 'http://www.omdbapi.com/?t=' + title + '&tomatoes=true&r=json';
+function getMovieInfo(titles, index, callback){
+  // takes the title of a movie and the index of it in the array
+  // adds an object containing movie data to the movieinfo array
+  url = 'http://www.omdbapi.com/?t=' + titles[index] + '&tomatoes=true&r=json';
   request(url, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       try{
-        movieInfo = JSON.parse(body);
+        movieInfo[index] = JSON.parse(body);
+        console.log('info added!');
       } catch(e){
         return console.error(e);
       }
-      callback(movie);
+      callback();
     } else if( error ){
       throw error;
     }
@@ -57,23 +64,40 @@ function getMovieInfo(title,callback){
 router.get('/:movie?', function(req, res) {
   //direct to home path or movie path, call render in movie info and text if movie
   if(req.path === '/'){
-    res.render('movies-error', {movieInfo : movieInfo});
+    res.render('movies-home');
   } else {
 
-    movie = req.params.movie;
-    // movie = movie.split('+');
-    getMovieInfo(movie, function(data){
-      if(movieInfo.Response === 'False'){
-        res.render('movies-error', {title : movie});
-      } else{
-        getMovieImage(data, function(info){
-          res.render('movies', {movieInfo: info});
-        });
-      } 
+    title = req.params.movie;
+    movies = title.split('+');
+
+    //reset values to zero
+    counter = 0;
+    movieInfo = [];
+    movieImages = [];
+
+    movies.forEach(function(movie, index){
+      movieInfo.push({});
+      movieImages.push([]);
+      console.log('pushed')
+      getMovieInfo(movies, index, function(){
+        if(movieInfo[index].Response === 'False'){
+          res.render('movies-error', {title : movie});
+        } else{
+            counter++;
+            if(counter === ( movies.length * 2 )){
+              res.render('movies', {movieInfo : movieInfo, images: movieImages});
+            }
+        }
+      }); // get movie info
+      getMovieImage(movies, index, function(){
+        counter++;
+        if(counter === ( movies.length * 2 )){
+          res.render('movies', {movieInfo : movieInfo, images: movieImages});
+        }
+      });
     });
   }
-
-});
+}); //router function;
 
 
 router.post('/', function(req, res){
